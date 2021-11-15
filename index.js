7a2ffe8a58eb3fc18e7654063ff9c920d91bc403
Rakesh-official <p_workspace@outlook.com>
2021-11-15 10:26:29 +0530

    New comit

---
+++ index.js
@@ -0,0 +1,13 @@
+document.getElementById("logout").onclick = function () {
+    location.href = "login.html";
+    };
+function signIn() {
+    location.href = "index.html";
+    };
+    
+function sidebarOpen() {
+    document.getElementById("navSide").classList.add("display-sidebar");
+    };
+function sidebarClose() {
+    document.getElementById("navSide").classList.remove("display-sidebar");
+    };
